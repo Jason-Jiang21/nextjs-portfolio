fbeee14273f329c52aefc851d89ef091b9425b55
Jason Jiang <jnjiang@usc.edu>
2024-07-26 23:48:23 -0700

    Fix

--- app/projects/Youtube.tsx
+++ app/projects/Youtube.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import Link from 'next/link';
-import PropTypes from "prop-types";
 
-const YoutubeEmbed = ({ embedId }) => (
-    <div className="video-responsive flex justify-center">
-      <iframe
-        width="853"
-        height="480"
-        src={`https://www.youtube.com/embed/${embedId}`}
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        title="Embedded youtube"
-      />
-    </div>
-  );
-  
-  YoutubeEmbed.propTypes = {
-    embedId: PropTypes.string.isRequired
-  };
-  
-  export default YoutubeEmbed;
+interface YoutubeEmbedProps {
+  embedId: string; // Define the type of embedId as a string
+}
+
+const YoutubeEmbed: React.FC<YoutubeEmbedProps> = ({ embedId }) => (
+  <div className="video-responsive flex justify-center">
+    <iframe
+      width="853"
+      height="480"
+      src={`https://www.youtube.com/embed/${embedId}`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="YouTube video player"
+    />
+  </div>
+);
+
+export default YoutubeEmbed;
